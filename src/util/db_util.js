28c939d80fb20cc044d53360e99552ed676367c4
Neeraj Dixit <neerajdixit27@gmail.com>
2017-06-06 22:07:35 +0530

    added schema doc

--- src/util/db_util.js
+++ src/util/db_util.js
@@ -1,5 +1,5 @@
 //db config file
-
+//id int, first_name varchar(60), last_name varchar(30), phone varchar(30), email varchar(30), role varchar(8) table schema
 import mysql from 'mysql';
 
 let connectionPool  =    mysql.createPool({
